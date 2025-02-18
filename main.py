15d44d3e2e94cb17cab3c683906881965d689874
sudocbrown99 <cmichaelbrown99@gmail.com>
2025-02-18 13:55:49 -0600

    Add Text Count

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 def main():
-  with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-    print(file_contents)
-main()
+  book_path = "books/frankenstein.txt"
+  text = get_book_text(book_path)
+  num_words = get_num_words(text)
+  print(num_words)
+
+def get_book_text(path):
+  with open(path) as f:
+    return f.read()
+  
+def get_num_words(text):
+  words = text.split()
+  return len(words)
