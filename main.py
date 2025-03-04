4702451cf7b7c1adfbb0c778559c19c7285ed0f2
sudocbrown99 <cmichaelbrown99@gmail.com>
2025-03-04 16:05:02 -0600

    Finsihed bookbot report

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
   text = get_book_text(book_path)
   num_words = get_num_words(text)
   chars_dict = get_chars_dict(text)
+  sorted_list = sort_chars(chars_dict)
   print(
     f"============ BOOKBOT ============\n"
     f"Analyzing book found at {book_path}..."
@@ -11,14 +12,16 @@ def main():
     f"----------- Word Count ----------\n"
     f"Found {num_words} total words"
     )
-  print(
-    f"--------- Character Count -------\n"
-    f""
-  )
+  print(f"--------- Character Count -------\n")
+  for char_dict in sorted_list:
+    print(f"{char_dict['char']}: {char_dict['count']}")
+  print("============= END ===============")
 
 from stats import get_num_words
 
 from stats import get_book_text
 
 from stats import get_chars_dict
+
+from stats import sort_chars
 main()
--- stats.py
+++ stats.py
@@ -16,3 +16,13 @@ def get_chars_dict(text):
     else:
       chars[lowered] = 1
   return chars
+
+def sort_chars(chars_dict):
+  chars_list = []
+  for char, count in chars_dict.items():
+    if char.isalpha():
+      chars_list.append({"char": char, "count": count})
+  
+  chars_list.sort(reverse=True, key=lambda x: x["count"])
+
+  return chars_list
